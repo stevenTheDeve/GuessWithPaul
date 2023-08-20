54bc8433920815729ccc5380f3672dc1e1f87486
stevenTheDeve <steventhedeve@gmail.com>
2023-08-20 00:03:47 -0700

    Initial Commit

---
+++ GuessWithPaul/GuessWithPaulApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessWithPaulApp.swift
+//  GuessWithPaul
+//
+//  Created by Estevan Hinojosa on 8/20/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessWithPaulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
